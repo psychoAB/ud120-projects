104eaf705113caf8bb6905e3307fef6bc641e8cd
psychoAB <dome.2499@hotmail.com>
2016-07-05 06:46:08 +0700

    change kernel to rbf and C = 10000.0

---
+++ svm/rbf_svm_author_id.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+
+from sklearn.svm import SVC
+
+classifier = SVC(C = 10000.0)
+
+t = time()
+classifier.fit(features_train, labels_train)
+trainingTime = round(time() - t, 3)
+print "training time: ", trainingTime, "s"
+
+t = time()
+result = classifier.predict(features_test)
+predictionsTime = round(time() - t, 3)
+print "predictions time: ", predictionsTime, "s"
+
+accuracy = round(classifier.score(features_test, labels_test), 3)
+print "accuracy: ", accuracy
+
+#########################################################
+
+

cd259885e73c7365e6cb55a13715bd24f480b776
psychoAB <dome.2499@hotmail.com>
2016-07-18 18:33:33 +0700

    count people, features/person, poi

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,14 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+n_people = len(enron_data)
+print "n_people:", n_people
 
+n_feature = len(enron_data.values()[0])
+print "n_feature:", n_feature
+
+n_poi = 0
+for person in enron_data:
+    if enron_data[person]["poi"] == 1:
+        n_poi += 1
+print "n_poi:", n_poi
